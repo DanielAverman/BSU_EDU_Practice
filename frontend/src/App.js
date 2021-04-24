973634724cb7fda25a40dfbeeeb0f65a356c932f
Daniil-com <66278019+Daniil-com@users.noreply.github.com>
2021-04-24 20:35:47 +0300

    test version
    updated frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+
+  return (
+    <div>
+    <h1>Hello world and you</h1>
+    <Button variant = "primary">Button</Button>
+    </div> 
+  );
+
+
+}
+export default App;
+
+
